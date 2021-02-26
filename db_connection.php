e4c6993a825cc147c4bb76a0f166b69c0ef99dd6
imilic17 <79484218+imilic17@users.noreply.github.com>
2021-02-26 10:45:01 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -8,20 +8,20 @@ $db=mysqli_connect ($server,$username,$password);
 
 
 if($db){
-    echo "Spojeni ste sa serverom <br>";
+   // echo "Spojeni ste sa serverom <br>";
 
     $db_selected=mysqli_select_db ($db,$Baza);
     
-    if($db_selected){
-        echo "Baza podataka je uspješno odabrana";
+    //if($db_selected){
+   //     echo "Baza podataka je uspješno odabrana";
     }
-    else{
-        echo "Došlo je do pogreške kod odabira baze";
-    }
-}
+   // else{
+   //     echo "Došlo je do pogreške kod odabira baze";
+   // }
+//}
 
-else{
-    echo "Došlo je do pogreške prilikom spajanja";
-}
+//else{
+   // echo "Došlo je do pogreške prilikom spajanja";
+//}
 
 ?>
