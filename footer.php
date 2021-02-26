c686b984186ebe307e1d62a0e15aa6ca114e6221
imilic17 <79484218+imilic17@users.noreply.github.com>
2021-02-27 00:14:38 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer class="footer">
+  <div class="columns is-variable is-4">
+    <div class="column is-one-third">
+      <br><br><br><br><br><br><br>
+      <a href="index.php">
+       <img src="Logo3.png" height="100" width="100"></a>
+    </div>
+    <div class="column is-one-third" id="OMeni">
+      <h3 class="title is-4">O meni</h3><br>
+      Zovem se Tia Lneniček, maturantica<br>
+      sam Tehničke škole Daruvar,<br>
+      smijer: Tehničar za računalstvo.<br>
+      Daljnje studiranje planiram nastaviti <br>
+      na Rochester Institute of Technology, <br>
+      smijer: Web and mobile computing u Zagrebu.<br><br>
+      Ukoliko imate bilo kakvih pitanja i savjeta<br> za daljnji razvoj 
+      i eventualno poboljšanje<br> stranice kontaktirajte me na:<br><br>
+       <a href = "mailto: tia.lnenicek@gmail.com" class="link">tia.lnenicek@gmail.com</a><br><br>
+       <a class="social" href="https://www.instagram.com/tia.lnenicek/?hl=en">
+        <img alt="instagram" src="instagram.png" width="30" height="30" ></a>
+        <a class="social" href="https://www.facebook.com/tia.lnenicek/">
+            <img alt="facebook" src="facebook.png" width="30" height="30"></a>
+    </div>
+    <div class="column is-one-third" id="OstaliKontakti">
+      <h3 class="title is-4">Ostali kontakti</h3><br>
+      <b>Studentski Centar u Zagrebu:</b><br><a href = "mailto: smjestaj@sczg.hr" class="link">smjestaj@sczg.hr</a><br><hr id="FooterMail">
+  <b>Studentska naselja:</b><br><br>
+  SN "Stjepan Radić"<br>
+  <a href = "mailto: snsradic@sczg.hr" class="link">snsradic@sczg.hr</a><br>
+  <br>
+  SN "Cvjetno naselje"<br>
+  <a href = "mailto: sncvjetno@sczg.hr" class="link">sncvjetno@sczg.hr</a><br>
+  <br>
+  SN "Dr. Ante Starčević"<br>
+  <a href = "mailto: snastarcevic@sczg.hr" class="link">snastarcevic@sczg.hr</a><br>
+  <br>
+  SN "Lašćina"<br>
+  <a href = "mailto: snlascina@sczg.hr" class="link">snlascina@sczg.hr</a>
+    </div>
+  </div>
+
+    <div class="content has-text-centered">
+      <br><br><br>
+      <p id="kontakt">Sva prava pridržana. &copy; 2021. </p>
+    </div>
+  </footer>
